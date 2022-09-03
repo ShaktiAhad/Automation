f0a7d1fd13234d90377c258bc9db8caef0f02b16
ShaktiAhad <ahadnoor80@gmail.com>
2022-09-04 01:19:23 +0900

    added aws-poc

--- aws-poc/vars/describe_stack_events.groovy
+++ aws-poc/vars/describe_stack_events.groovy
@@ -1,6 +1,6 @@
 def call(){
     withCredentials([aws(accessKeyVariable: 'AWS_ACCESS_KEY_ID', credentialsId: 'aws-cred', secretKeyVariable: 'AWS_SECRET_ACCESS_KEY')]){
         stack_events = command_functn("aws cloudformation describe-stack-events --region ap-northeast-1 --stack-name ${params.cf_stack_name}")["StackEvents"]
-        return stack_events[0]
+        return stack_events
     }
 }
