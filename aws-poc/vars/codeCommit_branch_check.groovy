2422fba2b9d7deec5f554588eadaeced155405d2
ShaktiAhad <ahadnoor80@gmail.com>
2022-09-04 00:14:13 +0900

    added aws-poc

--- aws-poc/vars/codeCommit_branch_check.groovy
+++ aws-poc/vars/codeCommit_branch_check.groovy
@@ -1,3 +1,5 @@
+#!/usr/bin/env groovy
+
 def call(){
     println (command_functn("aws codecommit get-branch --region ${env.AWS_DEFAULT_REGION} --repository-name ${params.code_commit_repo_name} --branch-name ${params.version}"))
 }
