328fef66358e3e8d532634f43ff60b8b5b13f87d
ShaktiAhad <ahadnoor80@gmail.com>
2022-09-04 09:01:09 +0900

    added aws-poc

--- aws-poc/vars/codeCommit_branch_check.groovy
+++ aws-poc/vars/codeCommit_branch_check.groovy
@@ -2,6 +2,6 @@
 
 def call(){
     withCredentials([aws(accessKeyVariable: 'AWS_ACCESS_KEY_ID', credentialsId: 'aws-cred', secretKeyVariable: 'AWS_SECRET_ACCESS_KEY')]){
-        println (command_functn("aws codecommit get-branch --region ap-northeast-1 --repository-name ${params.code_commit_repo_name} --branch-name ${params.version}"))
+        return (command_functn("aws codecommit get-branch --region ap-northeast-1 --repository-name ${params.code_commit_repo_name} --branch-name ${params.version}"))
     }
 }
--- aws-poc/vars/update_cf_stack.groovy
+++ aws-poc/vars/update_cf_stack.groovy
@@ -1,5 +1,5 @@
 def call(){
     withCredentials([aws(accessKeyVariable: 'AWS_ACCESS_KEY_ID', credentialsId: 'aws-cred', secretKeyVariable: 'AWS_SECRET_ACCESS_KEY')]){
-        println (command_functn("aws cloudformation update-stack --region ap-northeast-1 --stack-name s3-bucket-cf --use-previous-template --parameters ParameterKey=BucketPrefix,ParameterValue=${params.bucket_name} ParameterKey=Environment,ParameterValue=test --capabilities CAPABILITY_NAMED_IAM"))
+        return (command_functn("aws cloudformation update-stack --region ap-northeast-1 --stack-name s3-bucket-cf --use-previous-template --parameters ParameterKey=BucketPrefix,ParameterValue=${params.bucket_name} ParameterKey=Environment,ParameterValue=test --capabilities CAPABILITY_NAMED_IAM"))
     }
 }
