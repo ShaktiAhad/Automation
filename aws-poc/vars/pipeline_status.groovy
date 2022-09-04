ce8a708fb40464cfbdc2141d4da619d982ebf51b
ShaktiAhad <ahadnoor80@gmail.com>
2022-09-04 20:44:10 +0900

    added aws-poc

--- aws-poc/vars/pipeline_status.groovy
+++ aws-poc/vars/pipeline_status.groovy
@@ -1,7 +1,7 @@
 def call(){
     withCredentials([aws(accessKeyVariable: 'AWS_ACCESS_KEY_ID', credentialsId: 'aws-cred', secretKeyVariable: 'AWS_SECRET_ACCESS_KEY')]){
         get_exec_id = command_functn("aws codepipeline list-pipeline-executions --region ap-northeast-1 --pipeline-name ${params.code_pipeline_name}")["pipelineExecutionSummaries"]["pipelineExecutionId"][0]
-        pipeline_status = command_functn("aws codepipeline get-pipeline-execution --region ap-northeast-1 --pipeline-execution-id ${get_exec_id} --pipeline-name ${params.code_pipeline_name}")
+        pipeline_status = command_functn("aws codepipeline get-pipeline-execution --region ap-northeast-1 --pipeline-execution-id ${params.get_exec_id} --pipeline-name ${params.code_pipeline_name}")
         return pipeline_status["pipelineExecution"]["status"]
     }
 }
