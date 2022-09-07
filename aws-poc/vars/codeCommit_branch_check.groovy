a9651f0adfb7a831603e74d9c3907b7fcbfeaf36
ShaktiAhad <ahadnoor80@gmail.com>
2022-09-07 23:47:44 +0900

    added aws-poc

--- aws-poc/vars/codeCommit_branch_check.groovy
+++ aws-poc/vars/codeCommit_branch_check.groovy
@@ -1,5 +1,3 @@
-#!/usr/bin/env groovy
-
 def call(){
     // withCredentials([aws(accessKeyVariable: 'AWS_ACCESS_KEY_ID', credentialsId: 'aws-cred', secretKeyVariable: 'AWS_SECRET_ACCESS_KEY')]){
     return (command_functn("aws codecommit get-branch --region ap-northeast-1 --repository-name ${params.code_commit_repo_name} --branch-name ${params.version}"))
