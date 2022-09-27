d05896d45687ae70176f55ca070e150247a02469
ShaktiAhad <ahadnoor80@gmail.com>
2022-09-27 15:30:32 +0900

    fixed issue

--- aws-poc/vars/disable_eventRule.groovy
+++ aws-poc/vars/disable_eventRule.groovy
@@ -1,3 +1,3 @@
 def call(){
-    return (command_functn("aws events disable-rule --region ap-northeast-1 --name ${params.event_rule_name}"))
+    return ("aws events disable-rule --region ap-northeast-1 --name ${params.event_rule_name}")
 }
