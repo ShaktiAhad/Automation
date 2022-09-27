263abeeec1e0030d4b08dee80e4dbe381ae405e7
ShaktiAhad <ahadnoor80@gmail.com>
2022-09-27 15:45:22 +0900

    fixed issue

--- aws-poc/vars/command_functn.groovy
+++ aws-poc/vars/command_functn.groovy
@@ -3,6 +3,7 @@ import groovy.json.JsonSlurperClassic
 def call(aws_command){
     withCredentials([aws(accessKeyVariable: 'AWS_ACCESS_KEY_ID', credentialsId: 'aws-cred', secretKeyVariable: 'AWS_SECRET_ACCESS_KEY')]){
         def result = sh(script: "${aws_command}", returnStdout: true).trim()
+        println(result.class())
         if (result.length() == 0){
             println ("No json output.")
         }
