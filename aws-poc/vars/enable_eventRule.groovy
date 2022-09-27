3f160109cab9b5beaa185d15140488d1fcad63fb
ShaktiAhad <ahadnoor80@gmail.com>
2022-09-27 15:45:02 +0900

    fixed issue

--- aws-poc/vars/enable_eventRule.groovy
+++ aws-poc/vars/enable_eventRule.groovy
@@ -1,3 +1,3 @@
 def call(){
-    return (sh(script:"aws events enable-rule --region ap-northeast-1 --name ${params.event_rule_name}"))
+    return (command_functn("aws events enable-rule --region ap-northeast-1 --name ${params.event_rule_name}"))
 }
