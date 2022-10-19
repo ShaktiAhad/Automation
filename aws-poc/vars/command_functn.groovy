f91bc895b9b96ceb69f505e1810d7f3ee9736f1c
Ahad Noor Shakti <53283457+ShaktiAhad@users.noreply.github.com>
2022-10-19 17:28:38 +0900

    Update command_functn.groovy

--- aws-poc/vars/command_functn.groovy
+++ aws-poc/vars/command_functn.groovy
@@ -4,7 +4,8 @@ def call(aws_command){
 //     withCredentials([aws(accessKeyVariable: 'AWS_ACCESS_KEY_ID', credentialsId: 'aws-cred', secretKeyVariable: 'AWS_SECRET_ACCESS_KEY')])
     def credentialsFile = "/var/jenkins_home/credentials"
     withEnv(["AWS_SHARED_CREDENTIALS_FILE=${credentialsFile}"]){
-        def result = sh(script: "${aws_command}", returnStdout: true).trim()
+//         def result = sh(script: "${aws_command}", returnStdout: true).trim()
+        def result = shReturnStdout(aws_command)
         if (result.length() == 0){
             println (" ## No json output.")
         }
